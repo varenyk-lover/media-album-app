50ee67d428beba048973fb43da94106d09d6540b
Hanna Horbova <rockyeaaah@gmail.com>
2023-05-22 19:28:27 +0300

    Created thunks for responses.

---
+++ src/components/Skeleton.js
@@ -0,0 +1,10 @@
+import classNames from "classnames";
+
+function Skeleton({times}) {
+    const boxes = Array(times).fill(0).map((_, i) => {
+        return <div key={i}/>
+    });
+  return boxes;
+}
+
+export default Skeleton;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
     const response = await axios.get("http://localhost:30005/users");
+    //dev only!!!!!!!!!!!to delete at finish
+    await pause(1000);
     return response.data;
 });
+
+//dev only!!!!!!!!!!!to delete at finish
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
 
 export {fetchUsers};
