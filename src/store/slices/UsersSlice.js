9d71ee20ae9a566c4e71798ba42bd7c32676836d
Hanna Horbova <rockyeaaah@gmail.com>
2023-05-24 15:07:29 +0300

    Created ablility to delete user.

--- src/store/slices/UsersSlice.js
+++ src/store/slices/UsersSlice.js
@@ -46,8 +46,10 @@ const usersSlice = createSlice({
         });
         builder.addCase(removeUser.fulfilled, (state, action) => {
             state.isLoading = false;
-            //    FIX ME
-            console.log(action);
+          state.data
+              = state.data.filter((user) => {
+              return user.id !== action.payload.id;
+          });
         });
         builder.addCase(removeUser.rejected, (state, action) => {
             state.isLoading = false;
