9594870a71aeca0d3c35f32c44babfac7f73e52a
Hanna Horbova <rockyeaaah@gmail.com>
2023-05-26 17:51:47 +0300

    Rendered albums. Fixed bugs with updating sync rendering of new albums and skeleton UI.

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,7 +1,39 @@
+import {useFetchAlbumsQuery, useAddAlbumMutation} from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 
 function AlbumList({user}) {
+    const {data, error, isLoading} = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    }
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton className="h-10 w-full mt-3" times={3}/>
+    } else if (error) {
+        content = <div>Error loading albums</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>;
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos will be here
+            </ExpandablePanel>
+        });
+    }
+
     return (
-        <div>Album for {user.name}</div>
+        <div>
+            <div className="m-2 flex flex-row items-center justify-between">
+               <h3 className="text-lg font-bold">Album for {user.name}</h3>
+                <Button loading={results.isLoading} onClick={handleAddAlbum}>+ Add Album</Button>
+            </div>
+            <div>{content}</div>
+        </div>
     );
 }
 
---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,58 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {faker} from "@faker-js/faker";
+
+//dev only!!!!!!!!!!!to delete at finish
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    });
+};
+
+const albumsApi = createApi({
+    reducerPath: "albums",
+    baseQuery: fetchBaseQuery({
+        baseUrl: "http://localhost:30005",
+        //dev only!!!!!!!!!!!to delete at finish
+        fetchFn: async (...args) => {
+            await pause(1000);
+            return fetch(...args);
+        }
+    }),
+    endpoints(builder) {
+        return {
+            addAlbum: builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{type: "Album", id: user.id}];
+                },
+                query: (user) => {
+                    return {
+                        url: "/albums",
+                        method: "POST",
+                        body: {
+                            userId: user.id,
+                            title: faker.commerce.productName()
+                        }
+                    };
+                }
+            }),
+            fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    return [{type: "Album", id: user.id}];
+                },
+                query: (user) => {
+                    return {
+                        url: "/albums",
+                        params: {
+                            userId: user.id,
+                        },
+                        method: "GET",
+                    };
+                },
+            }),
+        };
+    },
+});
+
+
+export const {useFetchAlbumsQuery, useAddAlbumMutation} = albumsApi;
+export {albumsApi};
